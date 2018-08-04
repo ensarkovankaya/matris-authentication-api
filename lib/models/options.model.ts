62f7b27b1fefa7114fd109c1cdc7555fc87629e9
Ensar Kovankaya <ensar@kovankaya.com>
2018-08-04 19:28:35 +0300

    change url to baseURL

--- lib/models/options.model.ts
+++ lib/models/options.model.ts
@@ -2,7 +2,7 @@ import { IHttpClientModel } from "./http.client.model";
 import { ILoggerModel } from "./logger.model";
 
 export interface IOptions {
-    url?: string;
+    baseURL?: string;
     logger?: ILoggerModel;
     client?: IHttpClientModel;
     headers?: {[key: string]: string};
